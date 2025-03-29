10f2122fa178074bc7a641eb6101c4a3bb06ac25
Finn Pauls <mbp@pauls.dev>
2025-03-29 10:23:55 +0000

    Simplify bridges

--- src/components/Bridge.jsx
+++ src/components/Bridge.jsx
@@ -1,24 +1,12 @@
 import React from "react";
 import * as THREE from "three";
 
-const Bridge = ({ position, rotation = 0, scale = [1, 1, 1] }) => {
-  // Determine if this is a vertical bridge based on rotation
-  const isVertical = Math.abs(rotation - Math.PI / 2) < 0.01 || Math.abs(rotation - 3 * Math.PI / 2) < 0.01;
-  
-  // Adjust geometry args based on bridge orientation
-  // For vertical bridges, we need to swap width and depth
-  const geometryArgs = isVertical ? [1, 0.1, 1] : [1, 0.1, 1];
-  
+const Bridge = ({ position, scale = [1, 1, 1] }) => {
   return (
     <group position={position} rotation={[0, 0, 0]} scale={scale}>
-      {/* Bridge walkway with orientation-aware dimensions */}
       <mesh position={[0, 0.95, 0]}>
-        <boxGeometry args={geometryArgs} />
-        <meshStandardMaterial
-          color="#A9A9A9"
-          roughness={0.8}
-          metalness={0.2}
-        />
+        <boxGeometry args={[1, 0.1, 1]} />
+        <meshStandardMaterial color="#A9A9A9" roughness={0.8} metalness={0.2} />
       </mesh>
     </group>
   );
